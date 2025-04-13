1e2eef32a93092ef3af3b02da65040332fbffc1a
galyanrajat <galyanrajat@gmail.com>
2025-04-13 10:26:02 +0530

    update in demo,exception,logger,setup

---
+++ README.md
@@ -0,0 +1,16 @@
+# project discription
+
+
+
+# End-to-end-NLP-Project-Implementation
+
+
+## Project Workflows
+
+- constants
+- config_enity
+- artifact_enity
+- components
+- pipeline
+- app.py
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup # type: ignore
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Rajat Sharma",
+    author_email="galyanrajat@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
